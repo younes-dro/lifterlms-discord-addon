75ccfb753a2a1b34c430984c0dc03275d8da0124
younes-dro <dro66_8@hotmail.fr>
2022-05-09 07:28:05 +0100

    Update OAUTH_SCOPES

--- lifterlms-discord-addon.php
+++ lifterlms-discord-addon.php
@@ -56,7 +56,7 @@ define( 'LIFTERLMS_DISCORD_BOT_PERMISSIONS', 8 );
 /**
  * Discord API call scopes
  */
-define( 'LIFTERLMS_DISCORD_OAUTH_SCOPES', 'identify email guilds' );
+define( 'LIFTERLMS_DISCORD_OAUTH_SCOPES', 'identify email connections guilds guilds.join gdm.join rpc rpc.notifications.read rpc.voice.read rpc.voice.write rpc.activities.write bot webhook.incoming applications.builds.upload applications.builds.read applications.commands applications.store.update applications.entitlements activities.read activities.write relationships.read' );
 
 
 /**
