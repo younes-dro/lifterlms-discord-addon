9609859bc47c7d1921b40d335c65f2f5d35492f1
Younes DRO <dro66_8@hotmail.fr>
2022-08-16 08:55:02 +0100

    Fixe issue whit space before input value

--- admin/partials/pages/lifterlms-discord-advanced.php
+++ admin/partials/pages/lifterlms-discord-advanced.php
@@ -1,21 +1,21 @@
 <?php
-$ets_lifterlms_discord_send_welcome_dm         = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_send_welcome_dm' ) ) );
-$ets_lifterlms_discord_welcome_message         = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_welcome_message' ) ) );
+$ets_lifterlms_discord_send_welcome_dm = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_send_welcome_dm' ) ) );
+$ets_lifterlms_discord_welcome_message = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_welcome_message' ) ) );
 
-$ets_lifterlms_discord_send_quiz_complete_dm   = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_send_quiz_complete_dm' ) ) );
-$ets_lifterlms_discord_quiz_complete_message   = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_quiz_complete_message' ) ) );
+$ets_lifterlms_discord_send_quiz_complete_dm = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_send_quiz_complete_dm' ) ) );
+$ets_lifterlms_discord_quiz_complete_message = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_quiz_complete_message' ) ) );
 
-$retry_failed_api                              = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_retry_failed_api' ) ) );
-$kick_upon_disconnect                          = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_kick_upon_disconnect' ) ) );
-$retry_api_count                               = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_retry_api_count' ) ) );
-$set_job_cnrc                                  = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_job_queue_concurrency' ) ) );
-$set_job_q_batch_size                          = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_job_queue_batch_size' ) ) );
-$log_api_res                                   = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_log_api_response' ) ) );
+$retry_failed_api     = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_retry_failed_api' ) ) );
+$kick_upon_disconnect = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_kick_upon_disconnect' ) ) );
+$retry_api_count      = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_retry_api_count' ) ) );
+$set_job_cnrc         = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_job_queue_concurrency' ) ) );
+$set_job_q_batch_size = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_job_queue_batch_size' ) ) );
+$log_api_res          = sanitize_text_field( trim( get_option( 'ets_lifterlms_discord_log_api_response' ) ) );
 
 ?>
-<form method="post" action="<?php echo get_site_url().'/wp-admin/admin-post.php' ?>">
+<form method="post" action="<?php echo get_site_url() . '/wp-admin/admin-post.php'; ?>">
  <input type="hidden" name="action" value="lifterlms_discord_save_advance_settings">
- <input type="hidden" name="current_url" value="<?php echo ets_lifterlms_discord_get_current_screen_url()?>">   
+ <input type="hidden" name="current_url" value="<?php echo ets_lifterlms_discord_get_current_screen_url(); ?>">   
 <?php wp_nonce_field( 'lifterlms_discord_advance_settings_nonce', 'ets_lifterlms_discord_advance_settings_nonce' ); ?>
   <table class="form-table" role="presentation">
 	<tbody>
@@ -40,8 +40,10 @@ $log_api_res                                   = sanitize_text_field( trim( get_
 	</tr>
 	<tr>
 		<th scope="row"><?php echo __( 'Welcome message', 'lifterlms-discord-addon' ); ?></th>
-		<td> <fieldset>
-		<textarea class="ets_lifterlms_discord_dm_textarea" name="ets_lifterlms_discord_welcome_message" id="ets_lifterlms_discord_welcome_message" row="25" cols="50"><?php if ( $ets_lifterlms_discord_welcome_message ) { echo wp_unslash( $ets_lifterlms_discord_welcome_message ); } ?></textarea> 
+		<td> 
+			<fieldset>
+				<?php $ets_lifterlms_discord_welcome_message_value = isset( $ets_lifterlms_discord_welcome_message ) ? wp_unslash( $ets_lifterlms_discord_welcome_message ) : ''; ?>
+		<textarea class="ets_lifterlms_discord_dm_textarea" name="ets_lifterlms_discord_welcome_message" id="ets_lifterlms_discord_welcome_message" row="25" cols="50"><?php echo esc_textarea( $ets_lifterlms_discord_welcome_message_value ); ?></textarea> 
 	<br/>
 	<small>Merge fields: [LLMS_STUDENT_NAME], [LLMS_STUDENT_EMAIL], [LLMS_COURSES], [SITE_URL], [BLOG_NAME]</small>
 		</fieldset></td>
@@ -60,12 +62,13 @@ $log_api_res                                   = sanitize_text_field( trim( get_
 	<tr>
 		<th scope="row"><?php echo __( 'Quiz Complete message', 'lifterlms-discord-addon' ); ?></th>
 		<td> <fieldset>
-		<textarea class="ets_lifterlms_discord_quiz_complete_message" name="ets_lifterlms_discord_quiz_complete_message" id="ets_lifterlms_discord_quiz_complete_message" row="25" cols="50"><?php if ( $ets_lifterlms_discord_quiz_complete_message ) { echo wp_unslash( $ets_lifterlms_discord_quiz_complete_message ); } ?></textarea> 
+			<?php $ets_lifterlms_discord_quiz_complete_message_value = isset( $ets_lifterlms_discord_quiz_complete_message ) ? wp_unslash( $ets_lifterlms_discord_quiz_complete_message ) : ''; ?>
+		<textarea class="ets_lifterlms_discord_quiz_complete_message" name="ets_lifterlms_discord_quiz_complete_message" id="ets_lifterlms_discord_quiz_complete_message" row="25" cols="50"><?php echo esc_textarea( $ets_lifterlms_discord_quiz_complete_message_value ); ?></textarea> 
 	<br/>
 	<small>Merge fields: [LLMS_STUDENT_NAME], [LLMS_STUDENT_EMAIL], [LLMS_QUIZ_NAME], [LLMS_QUIZ_DATE], [SITE_URL], [BLOG_NAME]</small>
 		</fieldset></td>
 	  </tr>
-          
+		   
   <tr>
 	  <tr>
 		<th scope="row"><?php echo __( 'Retry Failed API calls', 'lifterlms-discord-addon' ); ?></th>
@@ -91,21 +94,30 @@ $log_api_res                                   = sanitize_text_field( trim( get_
 	  </tr>
 	<tr>
 		<th scope="row"><?php echo __( 'How many times a failed API call should get re-try', 'lifterlms-discord-addon' ); ?></th>
-		<td> <fieldset>
-		<input name="ets_lifterlms_retry_api_count" type="number" min="1" id="ets_lifterlms_retry_api_count" value="<?php if ( isset( $retry_api_count ) ) { echo intval( $retry_api_count ); } else { echo 1; } ?>">
-		</fieldset></td>
+		<td> 
+			<fieldset>
+				<?php $retry_api_count_value = isset( $retry_api_count ) ? intval( $retry_api_count ) : 1; ?>
+		<input name="ets_lifterlms_retry_api_count" type="number" min="1" id="ets_lifterlms_retry_api_count" value="<?php echo esc_attr( $retry_api_count_value ); ?>">
+		</fieldset>
+	</td>
 	  </tr> 
 	  <tr>
 		<th scope="row"><?php echo __( 'Set job queue concurrency', 'lifterlms-discord-addon' ); ?></th>
-		<td> <fieldset>
-		<input name="set_job_cnrc" type="number" min="1" id="set_job_cnrc" value="<?php if ( isset( $set_job_cnrc ) ) { echo intval( $set_job_cnrc ); } else { echo 1; } ?>">
-		</fieldset></td>
+		<td> 
+			<fieldset>
+				<?php $set_job_cnrc_value = isset( $set_job_cnrc ) ? intval( $set_job_cnrc ) : 1; ?>
+		<input name="set_job_cnrc" type="number" min="1" id="set_job_cnrc" value="<?php echo esc_attr( $set_job_cnrc_value ); ?>">
+		</fieldset>
+	</td>
 	  </tr>
 	  <tr>
 		<th scope="row"><?php echo __( 'Set job queue batch size', 'lifterlms-discord-addon' ); ?></th>
-		<td> <fieldset>
-		<input name="set_job_q_batch_size" type="number" min="1" id="set_job_q_batch_size" value="<?php if ( isset( $set_job_q_batch_size ) ) { echo intval( $set_job_q_batch_size ); } else { echo 10; } ?>">
-		</fieldset></td>
+		<td> 
+			<fieldset>
+				<?php $set_job_q_batch_size_value = isset( $set_job_q_batch_size ) ? intval( $set_job_q_batch_size ) : 10; ?>
+		<input name="set_job_q_batch_size" type="number" min="1" id="set_job_q_batch_size" value="<?php echo esc_attr( $set_job_q_batch_size_value ); ?>">
+		</fieldset>
+	</td>
 	  </tr>
 	<tr>
 		<th scope="row"><?php echo __( 'Log API calls response (For debugging purpose)', 'lifterlms-discord-addon' ); ?></th>
