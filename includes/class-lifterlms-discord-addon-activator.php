0ed3bab34b1e6ccc8cce817613e1580a4417344d
Younes DRO <dro66_8@hotmail.fr>
2022-08-17 07:07:28 +0100

    Add Lesson DM message defaults values

--- includes/class-lifterlms-discord-addon-activator.php
+++ includes/class-lifterlms-discord-addon-activator.php
@@ -32,11 +32,13 @@ class Lifterlms_Discord_Addon_Activator {
 	public static function activate() {
 		update_option( 'ets_lifterlms_discord_uuid_file_name', wp_generate_uuid4() );
 		update_option( 'ets_lifterlms_discord_send_welcome_dm', true );
-		update_option( 'ets_lifterlms_discord_welcome_message', 'Hi [LLMS_STUDENT_NAME] ([LLMS_STUDENT_EMAIL]), Welcome, Your courses [LLMS_COURSES] at [SITE_URL] Thanks, Kind Regards, [BLOG_NAME]' );               
-		update_option( 'ets_lifterlms_discord_send_quiz_complete_dm', true );                 
-		update_option( 'ets_lifterlms_discord_quiz_complete_message', 'Hi [LLMS_STUDENT_NAME] ([LLMS_STUDENT_EMAIL]), You have completed the quiz  [LLMS_QUIZ_NAME] at [LLMS_QUIZ_DATE] on website [SITE_URL], [BLOG_NAME]' );                
+		update_option( 'ets_lifterlms_discord_welcome_message', 'Hi [LLMS_STUDENT_NAME] ([LLMS_STUDENT_EMAIL]), Welcome, Your courses [LLMS_COURSES] at [SITE_URL] Thanks, Kind Regards, [BLOG_NAME]' );
+		update_option( 'ets_lifterlms_discord_send_lesson_complete_dm', true );
+		update_option( 'ets_lifterlms_discord_lesson_complete_message', 'Hi [LLMS_STUDENT_NAME] ([LLMS_STUDENT_EMAIL]), You have completed the Lesson  [LLMS_LESSON_NAME] at [LLMS_LESSON_DATE] on website [SITE_URL], [BLOG_NAME]' );
+		update_option( 'ets_lifterlms_discord_send_quiz_complete_dm', true );
+		update_option( 'ets_lifterlms_discord_quiz_complete_message', 'Hi [LLMS_STUDENT_NAME] ([LLMS_STUDENT_EMAIL]), You have completed the quiz  [LLMS_QUIZ_NAME] at [LLMS_QUIZ_DATE] on website [SITE_URL], [BLOG_NAME]' );
 		update_option( 'ets_lifterlms_discord_retry_failed_api', true );
-		update_option( 'ets_lifterlms_discord_kick_upon_disconnect', false ); 
+		update_option( 'ets_lifterlms_discord_kick_upon_disconnect', false );
 		update_option( 'ets_lifterlms_discord_retry_api_count', 5 );
 		update_option( 'ets_lifterlms_discord_job_queue_concurrency', 1 );
 		update_option( 'ets_lifterlms_discord_job_queue_batch_size', 6 );
@@ -45,7 +47,7 @@ class Lifterlms_Discord_Addon_Activator {
 		update_option( 'ets_lifterlms_discord_disconnect_button_bg_color', '#ff0000' );
 		update_option( 'ets_lifterlms_discord_loggedin_button_text', 'Connect With Discord' );
 		update_option( 'ets_lifterlms_discord_non_login_button_text', 'Login With Discord' );
-		update_option( 'ets_lifterlms_discord_disconnect_button_text', 'Disconnect From Discord' );                
+		update_option( 'ets_lifterlms_discord_disconnect_button_text', 'Disconnect From Discord' );
 	}
 
 }
