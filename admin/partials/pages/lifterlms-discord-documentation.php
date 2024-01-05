e449929a0cb27b08ced7a64bc9492553e7d03685
Younes DRO <dro66_8@hotmail.fr>
2024-01-05 10:05:06 +0100

    Added documentation links for enhanced user guidance.

--- admin/partials/pages/lifterlms-discord-documentation.php
+++ admin/partials/pages/lifterlms-discord-documentation.php
@@ -1,19 +1,19 @@
 <div class="ets-tutor">
 	<ul>
-		<!--<li>
-			<label><?php esc_html_e( 'YouTube video tutorial on how to connect the plugin with discord server', 'connect-lifterlms-discord' ); ?>
-			</label>
-			<a href="https://www.youtube.com/watch?v=2eHgq5BvDpU" target="_blank" ><?php echo __( 'Click Here', 'connect-lifterlms-discord' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
-		</li>-->
 		<li>
 			 <label><?php esc_html_e( 'Step-by-Step Written guide on how to connect the plugin with Discord server', 'connect-lifterlms-discord' ); ?>
 			</label>
-			<a href="https://www.expresstechsoftwares.com/" target="_blank" ><?php esc_html_e( 'Click Here', 'connect-lifterlms-discord' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>
+			<a href="https://www.expresstechsoftwares.com/step-by-step-guide-on-how-to-connect-lifterlms-website-with-discord-server/" target="_blank" ><?php esc_html_e( 'Click Here', 'connect-lifterlms-discord' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>
 		</li>
 		<li>
 			<label><?php esc_html_e( 'Features and other documentation', 'connect-lifterlms-discord' ); ?>
 			</label>
-			<a href="https://www.expresstechsoftwares.com/" target="_blank" ><?php esc_html_e( 'Click Here', 'connect-lifterlms-discord' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
+			<a href="https://www.expresstechsoftwares.com/discord-add-ons-documentation/" target="_blank" ><?php esc_html_e( 'Click Here', 'connect-lifterlms-discord' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
 		</li>
+		<li>
+			<label><?php esc_html_e( 'YouTube video tutorial on how to connect the plugin with discord server', 'connect-lifterlms-discord' ); ?>
+			</label>
+			<a href="https://youtu.be/v7lxB_Bvlv4" target="_blank" ><?php esc_html_e( 'Click Here', 'connect-lifterlms-discord' ); ?> <i class="fa fa-mouse-pointer" aria-hidden="true"></i></a>  
+		</li>		
 	</ul>
 </div>
--- includes/class-lifterlms-discord-addon.php
+++ includes/class-lifterlms-discord-addon.php
@@ -70,7 +70,7 @@ class Lifterlms_Discord_Addon {
 		if ( defined( 'LIFTERLMS_DISCORD_ADDON_VERSION' ) ) {
 			$this->version = LIFTERLMS_DISCORD_ADDON_VERSION;
 		} else {
-			$this->version = '1.0.7';
+			$this->version = '1.0.8';
 		}
 		$this->plugin_name = 'lifterlms-discord-addon';
 
